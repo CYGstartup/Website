0e412719d2551c646510b74da96255bc4207cd97
System <none>
2017-04-07 20:56:09 +0000

    🚋👕 Updated with Glitch

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Welcome to Choose Your Gender pre-alpha v0.0.5</h2>
-        <p>Please choose your gender.</p>
-        <Gender name="female" />
-        <Gender name="male" />
+        <div className="top">
+          <h2>Welcome to Choose Your Gender pre-alpha v0.0.5</h2>
+          <p>Please choose your gender.</p>
+          <Gender name="female" />
+          <Gender name="male" />
+        </div>
+        <div className="bottom">
+          <a href="ic8.link/13926">Page Icon</a>
+        </div>
       </div>
     );
   }
